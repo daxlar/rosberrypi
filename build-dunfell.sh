374830a2765d1a15918dd17ad2b58a274ad4a6bd
daxlar <azhang100@gmail.com>
2023-01-02 14:16:28 -0600

    fixed paths that get created when sourcing oe-init-build-env

--- build-dunfell.sh
+++ build-dunfell.sh
@@ -22,23 +22,19 @@ popd
 source ${SOURCES}/poky/oe-init-build-env ${BUILD_DIR}
 
 # setup bblayers.conf and local.conf within the build/build_dir/conf directory
-mkdir -p ${BUILD_DIR}/conf
-pushd ${BUILD_DIR}/conf
 # bblayers.conf 
-echo BBLAYERS += \"..\${SOURCES}/sources/meta-raspberrypi\" >> bblayers.conf || exit $?
-echo BBLAYERS += \"..\${SOURCES}/sources/meta-openembedded/meta-oe\" >> bblayers.conf || exit $?
-echo BBLAYERS += \"..\${SOURCES}/sources/meta-openembedded/meta-multimedia\" >> bblayers.conf || exit $?
-echo BBLAYERS += \"..\${SOURCES}/sources/meta-openembedded/meta-networking\" >> bblayers.conf || exit $?
-echo BBLAYERS += \"..\${SOURCES}/sources/meta-openembedded/meta-python\" >> bblayers.conf || exit $?
-echo BBLAYERS += \"..\${SOURCES}/sources/poky/meta\" >> bblayers.conf || exit $?
-echo BBLAYERS += \"..\${SOURCES}/sources/poky/meta-poky\" >> bblayers.conf || exit $?
-echo BBLAYERS += \"..\${SOURCES}/sources/poky/meta-yocto-bsp\" >> bblayers.conf || exit $?
-echo BBPATH := \"${TOPDIR}\" >> bblayers.conf || exit $?
-echo BBFILES ?= \"\" >> bblayers.conf || exit $?
+echo BBLAYERS += \"..\${SOURCES}/sources/meta-raspberrypi\" >> conf/bblayers.conf || exit $?
+echo BBLAYERS += \"..\${SOURCES}/sources/meta-openembedded/meta-oe\" >> conf/bblayers.conf || exit $?
+echo BBLAYERS += \"..\${SOURCES}/sources/meta-openembedded/meta-multimedia\" >> conf/bblayers.conf || exit $?
+echo BBLAYERS += \"..\${SOURCES}/sources/meta-openembedded/meta-networking\" >> conf/bblayers.conf || exit $?
+echo BBLAYERS += \"..\${SOURCES}/sources/meta-openembedded/meta-python\" >> conf/bblayers.conf || exit $?
+echo BBLAYERS += \"..\${SOURCES}/sources/poky/meta\" >> conf/bblayers.conf || exit $?
+echo BBLAYERS += \"..\${SOURCES}/sources/poky/meta-poky\" >> conf/bblayers.conf || exit $?
+echo BBLAYERS += \"..\${SOURCES}/sources/poky/meta-yocto-bsp\" >> conf/bblayers.conf || exit $?
 # local.conf
-echo "MACHINE ??= 'raspberrypi2'" >> local.conf || exit $?
-echo "BB_NUMBER_THREADS = \"11\"" >> local.conf || exit $?
-echo "PARALLEL_MAKE = \"-j 11 \"" >> local.conf || exit $?
+echo "MACHINE ?= 'raspberrypi2'" >> conf/local.conf || exit $?
+echo "BB_NUMBER_THREADS = \"11\"" >> conf/local.conf || exit $?
+echo "PARALLEL_MAKE = \"-j 11 \"" >> conf/local.conf || exit $?
 popd 
 
 bitbake core-image-base
